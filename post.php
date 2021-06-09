fc5877043e602dc574944c94532549f7632c9507
Aboddd199 <70726137+Aboddd199@users.noreply.github.com>
2021-06-09 17:28:20 +0300

    Create post.php

---
+++ post.php
@@ -0,0 +1,18 @@
+<?php
+// 𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐓𝐇𝐄 𝐅𝐈𝐒𝐇𝐈𝐍𝐆 𝐓𝐎𝐎𝐋 𝐀𝐂𝐂𝐎𝐔𝐍𝐓𝐒 𝐏𝐑𝐎𝐆𝐑𝐀𝐌𝐌𝐄𝐃 𝐁𝐘 @E_5_O @IIQQTQ كسم يلي بغير حرف واحد كسخت يلعب بالملف أو ياخد اتصالات أو يسحب اي شي اذا ما بيك شرف غير ✌️😂//
+// $config = json_decode(file_get_contents('config.json'),1);
+// $id = $config['id'];
+// $token = $config['token'];
+// include 'index.php';
+// $accounts = json_decode(file_get_contents('accounts.json'),1);
+// $id = $config['words'];
+// $file = $config['for'];
+// $a = file_exists('a') ? file_get_contents('a') : 'ap';
+// if($a == 'new'){
+// 	file_put_contents($file, '');
+// }
+// $ig = new ig(['account'=>$accounts[$file],'file'=>$file]);
+$get = file_get_contents('https://www.instagram.com/p/CB5RaX3l6Gk/');
+echo $get;
+// preg_match('/"id"\:"(.*?)"/i',$get,$m);
+// print_r($m); 
